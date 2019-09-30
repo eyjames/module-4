7ea60b3ee882a3c98f1b0d7e75a3e227627873f6
e. yvette j <54823220+eyjames@users.noreply.github.com>
2019-09-29 22:50:06 -0400

    jupyter notebook

---
+++ project water works.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+project water works
+
+
+# project water works
+
+# i am attempting to download csv file and to use genfromtxt to array the data set
+
+# In[2]:
+
+
+import csv
+csv file = (http://localhost:8888/edit/water)
+print(csvfile)
+
+
+# In[3]:
+
+
+import csv file
+open csv file = {http://localhost:8888/edit/water}
+print(csvfile)
+
+
+# In[4]:
+
+
+def_open csv file
+csv file = water
+import csv
+print(csvfile)
+
+
+# Well, that didn't work so I will move on to trying to use genfromtxt
+
+# In[5]:
+
+
+import numpy as np
+data = https://data.cityofnewyork.us/Environment/Water-Consumption-In-The-New-York-City/ia2d-e54m
+>>> np.genfromtxt(data, 0)
+print(np)
+
+
+# In[6]:
+
+
+#import numpy at np
+open data = (http://localhost:8888/edit/water)
+np.genfromtxt('data')
+
+
+# while I cannot fully execute code, I will however provide a part of the code relative to the function requested.
+# 
+
+# mean 
+# 
+# numpy.mean()
+
+# standard deviation
+# numpy.std()
+
+# In[ ]:
+
+
+
+
